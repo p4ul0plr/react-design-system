b55a1e756ee836fc82ccc611c8324c7dfa611e09
Paulo Nobrega <paulo.nobrega@mapesolutions.com>
2022-10-13 20:32:04 -0300

    added viteFinal

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -12,11 +12,11 @@ module.exports = {
   features: {
     storyStoreV7: true,
   },
-  /* viteFinal: (config, { configType }) => {
+  viteFinal: (config, { configType }) => {
     if (configType === "PRODUCTION") {
       config.base = "/react-design-system/";
     }
 
     return config;
-  }, */
+  },
 };
